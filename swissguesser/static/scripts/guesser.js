755f276f660d2d3a1c1ae2e54ad04ce70af410f4
Marc Monnerat <procrastinatio@gmail.com>
2015-01-20 15:51:37 +0100

    removed double getView()

--- swissguesser/static/scripts/guesser.js
+++ swissguesser/static/scripts/guesser.js
@@ -728,7 +728,7 @@ var guesser = {
 	// ### Animates transitions between points
 	startAnim: function(flyFrom, flyTo, flyExtent) {
 
-		var view = map.getView().getView();
+		var view = map.getView();
 
 		// Fly to the answer
 		setTimeout(function() {
